7ef693b93e1f89f701be85aa5a694e3f21867183
Everton <everton159@gmail.com>
2021-09-05 22:19:38 -0300

    Configurando e consumindo stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,9 @@
 import style from './styles.module.scss';
 
-export function SubscribeButton(){
+interface SubscribeButtonProps{
+  priceId:string;
+}
+export function SubscribeButton({priceId}:SubscribeButtonProps){
   return(
     <button
       type="button"
