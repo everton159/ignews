5fa970d4d3d346b2662c4d450b1cd8241cd48a2a
Everton <everton159@gmail.com>
2021-09-06 16:49:15 -0300

    configurações do stripe, redirects e sessions

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,35 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
 import style from './styles.module.scss';
 
 interface SubscribeButtonProps{
   priceId:string;
 }
 export function SubscribeButton({priceId}:SubscribeButtonProps){
+  const[session] =useSession();
+  async function  handleSubscribe(){
+      if(!session){
+        signIn('github')
+        return;
+      }
+
+      try{ 
+        const response =  await api.post('/subscribe')
+        console.log(response);
+        const {sessionId} = response.data;
+        const stripe = await getStripeJS()
+
+       await stripe.redirectToCheckout({sessionId})
+      }catch(err){
+        alert(err.message);
+      }
+  }
   return(
     <button
       type="button"
       className={style.subscribeButton}
+      onClick={handleSubscribe}
       >Subscribe now</button>
   )
 
