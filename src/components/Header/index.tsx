179789d5ba45dd25dd99aaf353bc32cbed388333
Everton <everton159@gmail.com>
2021-09-08 02:03:11 -0300

    Ajuste do activeLink e navegação entre paginas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link'
+import {useRouter} from 'next/router'
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import {ActiveLink} from '../ActiveLink';
 
 export function Header() {
+
+
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
           <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
           <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton/>
       </div>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
 import styles from './styles.module.scss';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client'
+import {RichText} from 'prismic-dom'
 
-export default function Posts() {
+
+type Post = {
+  slug:string;
+  title:string;
+  excerpt:string;
+  updatedAt:string;
+}
+
+interface PostProps {
+  posts:Post[]
+}
+
+export default function Posts({posts} : PostProps) {
   return (
     <>
       <Head>
@@ -9,25 +25,45 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Posts</strong>
-            <p>Paragrafo top do texto pra validar o que acontecer aqui</p>
-          </a>
-          <a>
-          <time>12 de março de 2021</time>
-            <strong>Posts</strong>
-            <p>Paragrafo top do texto pra validar o que acontecer aqui</p>
-          </a>
-          <a>
-          <time>12 de março de 2021</time>
-            <strong>Posts</strong>
-            <p>Paragrafo top do texto pra validar o que acontecer aqui</p>
-          </a>
+          {posts.map(post =>(
+            <a key = {post.slug} href="">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
 
       </main>
 
     </>
   );
+}
+
+
+export const getStaticProps: GetStaticProps = async()=>{
+const prismic = getPrismicClient(); 
+
+const response = await prismic.query([
+  Prismic.predicates.at('document.type', 'publication')],{
+    fetch:['publication.title','publication.content'],
+    pageSize:100,
+  })
+
+const posts =  response.results.map(post => {
+  return{
+    slug:post.uid,
+    title:RichText.asText(post.data.title),
+    excerpt:post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-Br',{
+      day :'2-digit',
+      month : 'long',
+      year :'numeric'
+    })
+  }
+})
+
+  return{ 
+  props:{posts}
+}
 }
