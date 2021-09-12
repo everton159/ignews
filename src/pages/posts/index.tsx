82b2e3bcb4d592740539c03d3d91b980f1bc4f4f
Everton <everton159@gmail.com>
2021-09-12 11:35:26 -0300

    gerando a leitura dinamica entre posts e conteudo

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,8 @@ import { getPrismicClient } from '../../services/prismic';
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client'
 import {RichText} from 'prismic-dom'
+import React from 'react';
+import Link from 'next/link';
 
 
 type Post = {
@@ -26,11 +28,13 @@ export default function Posts({posts} : PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post =>(
-            <a key = {post.slug} href="">
+            <Link href={`/posts/${post.slug}`}>
+            <a key = {post.slug}>
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
               <p>{post.excerpt}</p>
             </a>
+            </Link>
           ))}
         </div>
 
